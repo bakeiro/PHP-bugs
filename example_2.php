a7edbc8a965a427ba8ab129d0eb5a9a26dd89930
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-12-27 17:52:20 +0100

    new file

--- example_2.php
+++ example_2.php
@@ -1,13 +1,19 @@
 <?php
 
+//compare after deduct
 $x = 8 - 6.4;
 $y = 1.6;
 if($x === $y){
 	echo "they are equal!";
 }
+echo "x = ".$x."  y = ".$y."\n";
 
-echo "\n";
-echo $x;
 
-echo "\n";
-echo $y;
+//compare after floatval fun
+$num_1 = 0;
+$num_2 = floatval(0);
+
+if($num_1 === $num_2){
+	echo "they are equal!";
+}
+echo "num_1 = ".$num_1."  num_2 = ".$num_2."\n";
